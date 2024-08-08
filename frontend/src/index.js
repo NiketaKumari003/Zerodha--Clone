2a1466841d7c11afd3530552fef8180b1d5acacf
Niketa Kumari <niketa78709846@gmail.com>
2024-08-08 21:05:14 +0530

    modified files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,13 @@ import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/products/ProductPage";
+import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 
 import NotFound from "./landing_page/NotFound";
 import Navbar from "./landing_page/Navbar";
-
-
-
+import Footer from "./landing_page/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -22,14 +20,13 @@ root.render(
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
-      
       <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/products" element={<ProductPage />} />
+      <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  
+    <Footer />
   </BrowserRouter>
 );
